62813696ce2a6e2742184806753f953e4f42d797
Andrey Agarkov <agarkovand@gmail.com>
2024-01-29 00:16:11 +0100

    dependency management section added

--- service/src/main/java/dmdev/mentoring/Main.java
+++ service/src/main/java/dmdev/mentoring/Main.java
@@ -5,5 +5,6 @@ import static dmdev.mentoring.util.StringUtil.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, " + prepend("Wonderful!", null));
+        System.out.println(capitalize("capitalize each word in phrase"));
     }
 }
