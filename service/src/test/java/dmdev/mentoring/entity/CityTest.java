6895f0c74361f57893a98f65aabaae9f48619380
Andrey Agarkov <agarkovand@gmail.com>
2024-02-26 15:37:07 +0100

    Test native Query select city

--- service/src/test/java/dmdev/mentoring/entity/CityTest.java
+++ service/src/test/java/dmdev/mentoring/entity/CityTest.java
@@ -3,17 +3,14 @@ package dmdev.mentoring.entity;
 import dmdev.mentoring.entity.enums.Country;
 import org.junit.jupiter.api.Test;
 
+import static dmdev.mentoring.entity.enums.Country.POLAND;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CityTest extends AbstractEntityTest {
 
     @Test
     void testPersistence() {
-        City expected = City.builder()
-                .name("Kielce")
-                .region("Świętokrzyskie województwo")
-                .country(Country.POLAND)
-                .build();
+        City expected = createCity("Kielce", "Świętokrzyskie województwo", POLAND);
 
         session.persist(expected);
         session.evict(expected);
@@ -25,21 +22,33 @@ public class CityTest extends AbstractEntityTest {
 
     @Test
     void testHql() {
-        City expected = City.builder()
-                .name("Kielce")
-                .region("Świętokrzyskie województwo")
-                .country(Country.POLAND)
-                .build();
+        City expected = createCity("Kielce", "Świętokrzyskie województwo", POLAND);
 
         session.persist(expected);
         session.evict(expected);
 
-        var result = session
-                .createQuery("select c from City c where c.name = :cityName",
+        var hqlResult = session
+                .createQuery("select c from City c where c.name = :city",
                         City.class)
-                .setParameter("cityName", "Kielce")
+                .setParameter("city", "Kielce")
                 .list();
-        City actual = result.get(0);
+        City actual = hqlResult.get(0);
         assertThat(actual).isEqualTo(expected);
+
+        var nativeQueryResult = session
+                .createNativeQuery("select c.* from city c where c.name = :city",
+                        City.class)
+                .setParameter("city", "Kielce")
+                .list();
+        actual = nativeQueryResult.get(0);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static City createCity(String city, String region, Country country) {
+        return City.builder()
+                .name(city)
+                .region(region)
+                .country(POLAND)
+                .build();
     }
 }
